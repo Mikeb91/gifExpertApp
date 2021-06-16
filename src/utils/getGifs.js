4451046decdd255f5351eba4a3a625f48627eb0f
Mike Ballesteros <mikeb91@LAPTOP-09M2OK9T.localdomain>
2021-06-16 14:53:59 -0500

    Se corrige URL de petición para que sea https

--- src/utils/getGifs.js
+++ src/utils/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=WuM4HJU5aWmiRZ9mkmmSLU1xHqAZAck0`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=WuM4HJU5aWmiRZ9mkmmSLU1xHqAZAck0`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
